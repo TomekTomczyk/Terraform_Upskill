eda3ef5dc9945b27af6e6ead00d4531a379a3cc0
TomekTomczyk <tomasz.m.tomczyk@gmail.com>
2023-01-26 13:24:40 +0100

    SPRINT 4.1

--- environments/dev/instances.tf
+++ environments/dev/instances.tf
@@ -1,23 +1,3 @@
-module "aws_instance1" {
-  source                 = "../../modules/instances"
-  ami                    = data.aws_ami.ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.ttomczyk_ec2_security_group.id]
-  iam_instance_profile   = aws_iam_instance_profile.ttomczyk_aws_iam_instance_profile.name
-  subnet_id              = module.network.public_subnet1_id
-  instance_name          = "${var.infra_env}-ttomczyk-ecs-instance1"
-}
-
-module "aws_instance2" {
-  source                 = "../../modules/instances"
-  ami                    = data.aws_ami.ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [aws_security_group.ttomczyk_ec2_security_group.id]
-  iam_instance_profile   = aws_iam_instance_profile.ttomczyk_aws_iam_instance_profile.name
-  subnet_id              = module.network.public_subnet2_id
-  instance_name          = "${var.infra_env}-ttomczyk-ecs-instance2"
-}
-
 resource "aws_iam_instance_profile" "ttomczyk_aws_iam_instance_profile" {
   name = "${var.infra_env}-ttomczyk_aws_iam_instance_profile"
   role = aws_iam_role.ttomczyk_aws_iam_role.name
@@ -28,6 +8,7 @@ resource "aws_autoscaling_group" "ttomczyk_aws_autoscaling_group" {
   desired_capacity = 2
   min_size         = 2
   max_size         = 3
+  vpc_zone_identifier = [module.network.public_subnet1_id, module.network.public_subnet2_id]
 
   launch_template {
     id      = aws_launch_template.ttomczyk_aws_launch_template.id
@@ -57,11 +38,4 @@ resource "aws_launch_template" "ttomczyk_aws_launch_template" {
       Source = "Autoscaling"
     }
   }
-}
-
-resource "aws_db_instance" "ttomczyk_aws_db_instance" {
-  identifier = "ttomczyk_aws_db_instance"
-  storage_type = "gp2"
-  allocated_storage = 20
-  instance_class = "db.t2.micro"
 }
